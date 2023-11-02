b2c6d06b709bc6d5d697626c301a1dff526c6d50
miyamiyadai <143974725+miyamiyadai@users.noreply.github.com>
2023-11-02 09:06:12 +0000

    end

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  devise_for :users
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  
+  get "/homes/about" => "homes#about", as: "about"
+end
